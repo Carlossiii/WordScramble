2ea9a65e2a046cbd8f9509e4be1c7be9df70a486
Carlossiii <cv5673@gmail.com>
2022-09-12 19:47:38 -0300

    Finished project. A reset button was added with a score counter which multiplies the number of letters and words in the "right answer" list.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var usedWords = [""]
+    @State private var usedWords = [String] ()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var scoreNow = 0
+    @State private var scoreCount = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationView {
@@ -21,29 +28,110 @@ struct ContentView: View {
                 }
                 
                 Section {
+                    Text("Your score is: \(scoreNow)!")
+                }
+                
+                Section {
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle.fill")
-                        }
                             Text(word)
+                        }
                     }
                 }
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .toolbar {
+                Button ("New word", action: startGame)
+            }
+            .alert(errorTitle, isPresented: $showingError){
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
         
-        // Extra
+        guard answer.count > 2 && answer != rootWord else {
+            wordError(title: "Your word is too short or the same as the given one", message: "Go ahead and try again!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell tht word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
+        
         newWord = ""
+        
+        scoreCount = scoreCount + answer.count
+        scoreNow = scoreCount * usedWords.count
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = [String] ()
+                scoreCount = 0
+                scoreNow = 0
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from device.")
+    }
+    
+    func isOriginal (word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible (word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError (title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
